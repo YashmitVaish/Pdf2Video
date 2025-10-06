ba3fd6c52f7ddf99175375dc8744e023408b98b1
YashmitVaish <yashmitvaish4454@gmail.com>
2025-10-06 13:58:38 +0530

    start of revamp

--- test.py
+++
@@ -1,8 +0,0 @@
-import os 
-audio_path = "output_audio"
-contents = os.listdir(audio_path)
-for content in contents:
-        full_path = os.path.join(audio_path,content)
-        os.remove(full_path)
-
-os.rmdir(audio_path)
