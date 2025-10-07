61763dba029901c4a80a33bc1ae8907404921da7
YashmitVaish <yashmitvaish4454@gmail.com>
2025-10-07 12:34:52 +0530

    revamp 2.0

--- agents/base.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any
-
-class Agent(ABC):
-    @abstractmethod
-    def run(self,data,*arg,**kwargs)-> Any:
-        """Takes input data and returns processed output"""
